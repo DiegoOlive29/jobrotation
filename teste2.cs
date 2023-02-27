66aa2f547e5f97abadaf98c6dfa9fb6c1b42dfa4
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2023-02-27 17:41:39 -0300

    Resolução do teste 2

--- teste2.cs
+++ teste2.cs
@@ -9,22 +9,70 @@
 // 	b) Podem existir dias sem faturamento, como nos finais de semana e feriados. 
 //  Estes dias devem ser ignorados no cálculo da média;
 
-using System;
+
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.Runtime;
+using System;
+
 
 class Index{
 
     static void Main(){
-            int menorValor, maiorValor, maiorMedia;
+           
+        var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\dados.json");
 
-            var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\dados.json");
+        var resultJson = new DataContractJsonSerializer(typeof(List<Dia>));
+        var ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
 
-            Console.Write(Json)
-    
+        var dias = (List<Dia>)resultJson.ReadObject(ms);
         
+        int  contador =0;
+        double menorValor= dias[0].valor, maiorValor=0, maiorMedia =0, total= 0;
+
+           
+            for (int i = 0; i < 30; i++)
+            {
+                if (dias[i].valor > 0 ){
+                    contador += 1;
+                    total += dias[i].valor;
+
+                }
+
+                if (dias[i].valor > maiorValor){
+                    maiorValor = dias[i].valor;
+                }
+                if (dias[i].valor > 0 ){
+                   if(dias[i].valor < menorValor){
+                        menorValor =dias[i].valor;
+                   }
+
+                }
+            }
+
+            maiorMedia = total/contador;
+
+            int contadorDiaMedia=0;
+            for (int i = 0; i < 30; i++)
+            {
+                if(dias[i].valor > maiorMedia){
+                    contadorDiaMedia += 1;
+                }
+                
+            }
+
+            Console.WriteLine("Maior valor registrado "+ maiorValor.ToString("F"));
+            Console.WriteLine("Menor valor registrado "+ menorValor.ToString("F"));
+            Console.WriteLine("Quantidade de dias que o valor foi superior a média "+ contadorDiaMedia);
         }
+
+  
 }
+  public class Dia{
+            public int dia{get;set;}
+            public double valor{get;set;}
+    }
